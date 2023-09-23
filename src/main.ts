43d7b18feed67ff94bf2c4e6fd92f5036b0bd057
alievbat <aliev.bat@gmail.com>
2023-09-23 20:42:54 +0600

    nsklf

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Book Crud API')
+    .setDescription('Hiring testing task')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+
   await app.listen(3000);
+
+
 }
 bootstrap();
